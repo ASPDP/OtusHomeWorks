2a693000776de4673329307ddde9c7439dcaf148
Dmitriy Kruger <dokruger@ya.ru>
2025-01-28 17:31:38 +0500

    small fixes for 1st HW

--- charp2024_07_Kruger_homework_28/Program.cs
+++ charp2024_07_Kruger_homework_28/Program.cs
@@ -8,11 +8,12 @@ BenchmarkRunner.Run<Sums>();
 /// </summary>
 public class Sums
 {
-    private const int maximum = 10_000_000;
-    private static int[] _dataForBenchmark;
-    private static string _fileName = "kruger_homework_random_generated_data.txt"; // файл создадим в папке пользователя ~100 мб
+    private const string _fileName = "kruger_homework_random_generated_data.txt"; // файл создадим в папке пользователя ~100 мб
+    private const int _maximumArraySize = 10_000_000;
+
+    private static int[]? _dataForBenchmark;
     
-    [Params(100_000,1_000_000,maximum)]
+    [Params(100_000, 1_000_000, _maximumArraySize)]
     public int ArraySize;
 
     // https://benchmarkdotnet.org/articles/features/setup-and-cleanup.html
@@ -43,59 +44,56 @@ public class Sums
             //---------------------------------------------------------
             //              DATA GENERATION + SAVE TO FILE
             //---------------------------------------------------------
-            var mostBiggestArray = new int[maximum];
-            for (int i = 0; i < maximum; i++)
-                mostBiggestArray[i] = new Random().Next();
+            var mostBiggestArray = new int[_maximumArraySize];
+            for (int i = 0; i < _maximumArraySize; i++)
+                mostBiggestArray[i] = new Random().Next(int.MinValue,int.MaxValue);
             var lines = Array.ConvertAll(mostBiggestArray, element => element.ToString());
             File.WriteAllLines(filePath, lines);
             //---------------------------------------------------------
             //           INIT ARRAY FOR TARGETED MEASUREMENT 
             //---------------------------------------------------------
-            _dataForBenchmark = mostBiggestArray[..(ArraySize)];
-        } 
+            _dataForBenchmark = mostBiggestArray[..ArraySize];
+        }
     }
 
-
+    /// <summary>
+    /// Обычное
+    /// </summary>
     [Benchmark] 
-    public void CommonFor()
+    public void Common()
     {
         long result = 0; // даже если все int будут равны 2147483647, то их сумма будет меньше макс. знач. long
         foreach (var element in _dataForBenchmark)
             result += element;
-        
-        //Console.WriteLine($"real size: {_dataForBenchmark.Length,10} - expected size: {ArraySize,10} - {"CommonWOLinq",15} - {result}");
+
+        Log(result, "Common");
     }
 
-    [Benchmark] 
-    public void CommonLinq()
-    {
-        var result = _dataForBenchmark.AsParallel().Sum(e=>(long)e);
-        
-        //Console.WriteLine($"real size: {_dataForBenchmark.Length,10} - expected size: {ArraySize,10} - {"CommonLinq",15} - {result}");
-    }
-    
+    /// <summary>
+    /// Параллельное (для реализации использован Thread в List)
+    /// </summary>
     /// <param name="threads">0 mean 1 thread for 1 core</param>
     [Benchmark]
     [Arguments(2)]
-    [Arguments(7)]
+    [Arguments(7)] // самый быстрый для моего компа при 10 млн элементов
     [Arguments(0)]
     public void Thread(int threads)
     {
         if (threads == 0)
             threads = Environment.ProcessorCount;
-        var partSize = ArraySize / threads; // размер куска массива
+        var partSize = ArraySize / threads; // размер куска массива для передачи в тред
         var threadList = new List<Thread>();
         long result = 0;
         var lockobject = new object();
 
-        for (int iterationForNewThread = 0; iterationForNewThread < threads; iterationForNewThread++)
+        for (int i = 0; i < threads; i++)
         {
-            var i = iterationForNewThread; // исключаем захват переменной тредом
+            var iteration = i; // исключаем захват переменной тредом
             var thread = new Thread(() =>
                 {
-                    var from = partSize * i;
-                    var to = (threads - i == 1) ? ArraySize : partSize * (i+1);
-                    //       ^посл. итерация?     ^до конца   ^до конца куска массива
+                    var from = partSize * iteration;
+                    var to = (threads - iteration == 1) ? ArraySize : partSize * (iteration+1);
+                    //       ^последняя итерация?         ^до конца   ^до конца куска массива
 
                     long threadResult = 0;
                     for (long j = from; j < to; j++)
@@ -110,15 +108,37 @@ public class Sums
 
         foreach (var thread in threadList)
             thread.Join();
-        
-        //Console.WriteLine($"real size: {_dataForBenchmark.Length,10} - expected size: {ArraySize,10} - {"ThreadManual",15} - {result}");
-    }
 
+        Log(result, "Thread");
+    }
+    
+    /// <summary>
+    /// Параллельное с помощью LINQ (без партишининга)
+    /// </summary>
     [Benchmark]
     public void Plinq()
     {
+        // для решения проблемы с переполнением
+        // и ускорения, вместо Plinq
+        // можно глянуть в сторону https://github.com/DragonSpit/HPCsharp
         var result = _dataForBenchmark.AsParallel().Sum(e => (long)e);
 
-        //Console.WriteLine($"real size: {_dataForBenchmark.Length,10} - expected size: {ArraySize,10} - {"Plinq",15} - {result}");
+        Log(result, "Plinq");
+    }
+    
+    private void Log(long result, string methName)
+    {
+        // раскомментировать если хочется посмотреть правильно ли суммируется 
+        /*
+        Console.WriteLine(
+            $"""
+             ┌ real size:     {_dataForBenchmark.Length,20}
+             | expected size: {ArraySize,20}
+             | meth name:     {methName,20}
+             | frst element:  {_dataForBenchmark.First(),20}
+             | last element:  {_dataForBenchmark.Last(),20}
+             └ summ: {result}
+             """);
+         */
     }
 }
